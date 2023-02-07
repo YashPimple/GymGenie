897238b80c66c69a4363341fb3c654bd431377e9
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-02-07 22:35:56 +0530

    Update fetchData.js

--- src/utilis/fetchData.js
+++ src/utilis/fetchData.js
@@ -1,9 +1,18 @@
-import React from 'react'
 
-const fetchData = () => {
-  return (
-    <div>fetchData</div>
-  )
-}
 
-export default fetchData
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+  },
+};
+
+
+
+export const fetchData = async (url, options) =>{
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data
+};
